1f18c580b9ebddb80e98a9c8611e73959b7718bf
JaesonLoke <huanglulu28@gmail.com>
2021-10-29 23:07:24 +0800

    Ask before create new session

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -51,60 +51,6 @@ namespace WinFormsApp1
         private void BtnSelect_Click(object sender, EventArgs e)
         {
 
-            string path = @"Upload\";
-            if (Directory.GetDirectories(path).Length == 0)
-            {
-                OpenFileDialog openFileDialog = new OpenFileDialog();
-
-                //set initial directory for the 'select folder'
-                openFileDialog.InitialDirectory = @"C:\";
-                openFileDialog.Multiselect = true;
-                openFileDialog.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp;*.png)|*.jpg; *.jpeg; *.gif; *.bmp; *.png";
-
-
-
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    //format current date and time
-                    string currentDateTime = DateTime.Now.ToString("dd-MM-yyyy_HHmmss");
-
-                    //create new folder named as current date and time
-                    string UploadFolder = Path.Combine(@"Upload\", currentDateTime);
-                    Directory.CreateDirectory(UploadFolder);
-
-                    //create pass folder under newly created folder
-                    string passFolder = Path.Combine(UploadFolder, "Pass");
-                    Directory.CreateDirectory(passFolder);
-
-                    //create fail folder under newly created folder
-                    string failFolder = Path.Combine(UploadFolder, "Fail");
-                    Directory.CreateDirectory(failFolder);
-
-                    //get last modified folder
-                    DirectoryInfo latestFolder = new DirectoryInfo(@"Upload\").GetDirectories().OrderByDescending(d => d.LastWriteTimeUtc).First();
-
-                    foreach (string fileName in openFileDialog.FileNames)
-                    {
-                        try
-                        {
-                            File.Copy(fileName, Path.Combine(latestFolder.ToString(), Path.GetFileName(fileName)));
-                        }
-
-                        catch (Exception ex)
-                        {
-                            MessageBox.Show("Reported error: " + ex.Message);
-                        }
-                    }
-
-
-
-                    Form4 fm = new Form4();
-                    fm.Show();
-                    this.Hide();
-                }
-            }
-            else
-            {
                 DialogResult dialogResult = MessageBox.Show("Please be careful if u have any session undone yet. \n" +
                 "By clicking 'Yes', you will unable to resume the last session.\n\n" +
                 "Are you sure you want to continue?", "Creating New Session", MessageBoxButtons.YesNo);
@@ -166,6 +112,5 @@ namespace WinFormsApp1
                     //do something else
                 }
             }
-        }
     }
 }
